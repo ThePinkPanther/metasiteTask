470ec29bb9e1b5bbf57a2c4c7ead3783a946a99d
ben <rbjacikevicius@gmail.com>
2015-09-06 18:05:07 +0300

    Added a method call to FilterService from controller

--- src/main/java/metasiteTask/controllers/UserFiles.java
+++ src/main/java/metasiteTask/controllers/UserFiles.java
@@ -1,13 +1,19 @@
 package metasiteTask.controllers;
 
 import metasiteTask.Application;
+import metasiteTask.exceptions.SynchronousProcedureAlreadyRunningException;
+import metasiteTask.services.FileService;
+import metasiteTask.services.WordFilterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import metasiteTask.services.FileService;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @author ben
- * @version 1.0
+ * @version 1.1
  */
 
 @RestController
@@ -17,4 +23,16 @@ public class UserFiles extends FileControllerBase {
     protected FileService getFileService() {
         return Application.getUserFileService();
     }
+
+    @RequestMapping(value = "/generate", method = GET)
+    public ResponseEntity<Void> generateFiles() {
+        WordFilterService service = Application.getFilterService();
+        try {
+            service.start();
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        } catch (SynchronousProcedureAlreadyRunningException e) {
+            return new ResponseEntity<Void>(HttpStatus.LOCKED);
+        }
+    }
+
 }
